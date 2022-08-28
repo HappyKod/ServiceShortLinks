1831638a1aa61e0f7e864a37e94c45e19cad156d
Udin <sav.iudin@yandex.ru>
2022-08-28 21:47:20 +0300

    Изменено Эндпоинт GET /:id/

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -8,7 +8,7 @@ import (
 // Router указание маршртуов сревера
 func Router() *http.Server {
 	r := gin.New()
-	r.GET("/:id", func(context *gin.Context) { GivHandler(context) })
+	r.GET("/:id/", func(context *gin.Context) { GivHandler(context) })
 	r.POST("/", func(context *gin.Context) { PutHandler(context) })
 	server := http.Server{
 		Handler: r,
