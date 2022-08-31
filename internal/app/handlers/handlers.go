65282ac43ffbaa661139d626d042af5ee5c83aba
Udin <sav.iudin@yandex.ru>
2022-08-31 16:59:45 +0300

    Изменен вызов Хендлеров

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -7,7 +7,7 @@ import (
 // Router указание маршртуов сревера
 func Router() *gin.Engine {
 	r := gin.New()
-	r.GET("/:id", func(context *gin.Context) { GivHandler(context) })
-	r.POST("/", func(context *gin.Context) { PutHandler(context) })
+	r.GET("/:id", GivHandler)
+	r.POST("/", PutHandler)
 	return r
 }
