417489a379c6fa4f4c0284394790241d2b9374f2
Udin <sav.iudin@yandex.ru>
2022-09-25 22:45:52 +0300

    добавлено подержание конфигурирование сервиса с помощью флагов командной строки наравне с уже имеющимися переменными окружения

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,15 +5,23 @@ import (
 	"HappyKod/ServiceShortLinks/internal/app/handlers"
 	"HappyKod/ServiceShortLinks/internal/app/server"
 	"HappyKod/ServiceShortLinks/internal/models"
+	"flag"
+	"fmt"
 	"github.com/caarlos0/env/v6"
 	"log"
 )
 
 func main() {
 	var cfg models.Config
+	fmt.Println(cfg)
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatal(err, "Ошибка считывания конфига")
 	}
+	flag.StringVar(&cfg.Address, "a", cfg.Address, "адрес запуска HTTP-сервера")
+	flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "базовый адрес результирующего сокращённого URL")
+	flag.StringVar(&cfg.FileStoragePATH, "f", cfg.FileStoragePATH, "путь до файла с сокращёнными URL")
+	flag.Parse()
+	fmt.Println(cfg)
 	err := container.BuildContainer(cfg)
 	if err != nil {
 		log.Fatal(err)
