b67e286fdd6007062434cf341c6c5dbcb9ba775e
Saveliy Yudin <yudinsv@agata-hub.ru>
2022-12-27 13:04:38 +0300

    fear: добавить benchmark

---
+++ utils/utils_test.go
@@ -0,0 +1,20 @@
+package utils
+
+import "testing"
+
+func BenchmarkValidatorURL(b *testing.B) {
+	text := "https://github.com/HappyKod/ServiceShortLinks"
+	b.Run(b.Name(), func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ValidatorURL(text)
+		}
+	})
+}
+
+func BenchmarkGeneratorStringUUID(b *testing.B) {
+	b.Run(b.Name(), func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			GeneratorStringUUID()
+		}
+	})
+}
