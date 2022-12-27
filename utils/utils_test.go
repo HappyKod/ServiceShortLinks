4591af41d63e170d4752691b46a47c0ca4dbc002
Saveliy Yudin <yudinsv@agata-hub.ru>
2022-12-27 13:16:44 +0300

    fix: востановаить работоспасобность тестов

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -5,6 +5,7 @@ import "testing"
 func BenchmarkValidatorURL(b *testing.B) {
 	text := "https://github.com/HappyKod/ServiceShortLinks"
 	b.Run(b.Name(), func(b *testing.B) {
+		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			ValidatorURL(text)
 		}
@@ -12,8 +13,8 @@ func BenchmarkValidatorURL(b *testing.B) {
 }
 
 func BenchmarkGeneratorStringUUID(b *testing.B) {
-	b.ReportAllocs()
 	b.Run(b.Name(), func(b *testing.B) {
+		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
 			GeneratorStringUUID()
 		}
